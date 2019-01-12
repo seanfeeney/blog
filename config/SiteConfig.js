27883d3b8e2c842b3f77fa690a361da81b386d73
Sean Feeney <feeney22@gmail.com>
2019-01-12 03:24:36 +0000

    changing button colors

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Minimal Blog', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteUrl: 'https://minimal-blog.netlify.com', // Domain of your site. No trailing slash!
+  siteTitle: 'One of Those Blogs', // Navigation and Site Title
+  siteTitleAlt: 'One of Those Blogs', // Alternative Site title for SEO
+  siteUrl: 'https://theseanfeeney.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
@@ -12,9 +12,9 @@ module.exports = {
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@minimal', // Twitter Username - Optional
-  ogSiteName: 'minimal', // Facebook Site Name - Optional
-  ogLanguage: 'en_US', // Facebook Language
+  userTwitter: '', // Twitter Username - Optional
+  ogSiteName: '', // Facebook Site Name - Optional
+  ogLanguage: '', // Facebook Language
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
