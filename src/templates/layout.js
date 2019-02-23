8fc406c997403866e3278a76c9ef4bdfff0e3145
Sean Feeney <feeney22@gmail.com>
2019-02-23 21:09:55 +0000

    updating syled components and moving around layout.js

--- src/templates/layout.js
+++ src/templates/layout.js
@@ -1,10 +1,69 @@
-import React from 'react';
-import 'prismjs/themes/prism.css';
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+import styledNormalize from 'styled-normalize'
+import 'prismjs/themes/prism.css'
 
-import '../global-styles';
-import userConfig from '../../config';
+import userConfig from '../../config'
 
-import Header from '../components/Header';
+import Header from '../components/Header'
+
+const GlobalStyle = createGlobalStyle`
+  ${styledNormalize}
+  
+  @import url("https://fonts.googleapis.com/css?family=Roboto:400,700");
+  
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  html {
+    font-size: 62.5%;
+    -webkit-text-size-adjust: 100%;
+    -ms-text-size-adjust: 100%;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #f9fafc;
+    font-family: 'Open Sans', sans-serif; 
+    line-height: 1.5;
+    padding: 50px 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  .gatsby-highlight {
+    border-bottom: 1px solid #e0e6ed;
+    border-top: 1px solid #e0e6ed;
+    margin: 15px -100px;
+    padding: 0;
+
+    pre[class*="language-"] {
+      margin: 0;
+      padding: 25px 100px;
+    }
+  }
+
+  pre[class*="language-"] {
+    background: rgba(245, 245, 245, 1);
+    color: rgb(65, 76, 94);  }
+
+  @media only screen and (max-width: 870px) {
+    .gatsby-highlight {
+      margin: 15px -15px;
+
+      pre[class*="language-"] {
+        padding: 25px;
+      }
+    }
+  }
+`
 
 class Template extends React.Component {
   render() {
